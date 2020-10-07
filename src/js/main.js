104e6b9639b9dc0cdb073e2edaa0c45d4206a453
Fabian Alleblas <fabian@alleblas.de>
2020-10-07 15:54:02 +0200

    Update HTML CSS and JS

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+const questionContent = document.querySelector('[data-js=question-content]')
+const bookmarkContent = document.querySelector('[data-js=bookmark-content]')
+const createContent = document.querySelector('[data-js=create-content]')
+const profileContent = document.querySelector('[data-js=profile-content]')
+// NAV-ICONS
+const questionIcon = document.querySelector('[data-js=question-icon]')
+const bookmarkIcon = document.querySelector('[data-js=bookmark-icon]')
+const createIcon = document.querySelector('[data-js=create-icon]')
+const profileIcon = document.querySelector('[data-js=profile-icon]')
