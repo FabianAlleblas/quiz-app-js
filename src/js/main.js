2007e5dd3c2e72294689b3ce33f434cfeded37ac
Fabian Alleblas <fabian@alleblas.de>
2020-10-08 11:00:43 +0200

    Add getModule JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,27 @@
 //HEADINGS
-const questionHeading = document.querySelector('[data-js=question-heading]')
-const bookmarkHeading = document.querySelector('[data-js=bookmark-heading]')
-const createHeading = document.querySelector('[data-js=create-heading]')
-const profileHeading = document.querySelector('[data-js=profile-heading]')
+const questionHeading = getModule('question-heading')
+const bookmarkHeading = getModule('bookmark-heading')
+const createHeading = getModule('create-heading')
+const profileHeading = getModule('profile-heading')
 //CONTENTBOX
-const questionContent = document.querySelector('[data-js=question-content]')
-const bookmarkContent = document.querySelector('[data-js=bookmark-content]')
-const createContent = document.querySelector('[data-js=create-content]')
-const profileContent = document.querySelector('[data-js=profile-content]')
+const questionContent = getModule('question-content')
+const bookmarkContent = getModule('bookmark-content')
+const createContent = getModule('create-content')
+const profileContent = getModule('profile-content')
 // NAV-ICONS
-const questionNavIcon = document.querySelector('[data-js=question-navicon]')
-const bookmarkNavIcon = document.querySelector('[data-js=bookmark-navicon]')
-const createNavIcon = document.querySelector('[data-js=create-navicon]')
-const profileNavIcon = document.querySelector('[data-js=profile-navicon]')
+const questionNavIcon = getModule('question-navicon')
+const bookmarkNavIcon = getModule('bookmark-navicon')
+const createNavIcon = getModule('create-navicon')
+const profileNavIcon = getModule('profile-navicon')
 // CARD-BOOKMARK-ICON
 const bookmarkCardIcon = document.querySelectorAll(
   '[data-js=bookmark-cardicon]'
 )
 // FORM-BUTTON
-const resetButton = document.querySelector('[data-js=reset-btn]')
+const resetButton = getModule('reset-btn')
 // ANSWER-TOGGLE
-const toggleButton = document.querySelector('[data-js=toggle-btn]')
-const answerCard = document.querySelector('[data-js=answer-card]')
+const toggleButton = getModule('toggle-btn')
+const answerCard = getModule('answer-card')
 // EVENT-LISTENER
 questionNavIcon.addEventListener('click', () => {
   hideAllPages()
@@ -65,6 +65,11 @@ toggleButton.addEventListener('click', () =>
   answerCard.classList.toggle('hidden')
 )
 // FUNCTIONS
+function getModule(name) {
+  const selector = `[data-js=${name}]`
+  return document.querySelector(selector)
+}
+
 function hideAllPages() {
   questionHeading.classList.add('hidden')
   bookmarkHeading.classList.add('hidden')
